ba63fae6f452c5330bbe3f6328f338f0170b7c42
Christopher Gutteridge Real <cjg@macadmins-MacBook-Pro.local>
2022-12-20 13:28:53 +0000

    day20

---
+++ day20/pt1.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+import time
+import sys
+import re
+import copy
+from pprint import pprint,pformat
+
+t0 = seconds = time.time()
+tn = t0
+
+def timecheck():
+    global tn
+    global t0
+    new_time = time.time()
+    r = int(new_time-tn)
+    a = int(new_time-t0)
+    tn=new_time
+    return f'section_t={r} since_start={a}'
+
+file = 'data'
+if( len(sys.argv) == 2  and sys.argv[1] == '-t'):
+    file = 'test'
+    y = 10
+if( len(sys.argv) == 2  and sys.argv[1] == '-t2'):
+    file = 'test2'
+
+with open(file, 'r') as f:
+	lines = f.read().split("\n")
+
+def showring(ring):
+    v = [str(ring[0][0])]
+    i = ring[0][2]
+    while i!=0:
+        v.append(str(ring[i][0]))
+        i = ring[i][2]
+    print( " ".join( v ))
+
+
+
+# store [ value, previndex, nextindex ]
+ring = []
+i=0
+for line in lines:
+    if line=="":
+        continue
+    v = int(line)
+    print(v)
+    if v==0:
+        zero_index = i
+    ring.append([v,i-1,i+1])
+    i+=1
+size=len(ring)
+ring[0][1] = size-1
+ring[size-1][2] = 0
+
+pprint(ring)
+# mix it up
+for mix_index in range(0,size):
+   # pprint(ring)
+    #showring(ring)
+    item = ring[mix_index]
+    print( f'MOVING {item[0]}')
+    if item[0]==0:
+        continue
+
+    # remove item from the ring
+    # heal the gap
+    ring[ item[1] ][2] = item[2] # prev's next becomes item's next
+    ring[ item[2] ][1] = item[1] # next's prev becomes item's prev
+
+    # 2 forward   1 backwards
+    dir = 2
+    moves = item[0]
+    if item[0]<0:
+        dir = 1
+        moves = -item[0]
+        moves+= 1 # move one extra backwards so we always insert AFTER the final spot
+    i = mix_index
+    while moves > 0:
+        i = ring[i][dir]
+        moves-=1
+
+    # put the item back into the ring AFTER the item with index i
+    prv = i
+    nex = ring[i][2]
+    ring[mix_index][1]=prv
+    ring[mix_index][2]=nex
+    ring[prv][2] = mix_index
+    ring[nex][1] = mix_index
+
+        
+
+pprint(ring)
+showring(ring)
+print(zero_index)
+
+answer = 0
+
+i = zero_index
+for j in range(0,3):
+    for k in range(0,1000):
+        i = ring[i][2]
+    print( ring[i][0] )
+    answer += ring[i][0]
+
+
+
+print( f'PART 1 = {answer}' )
+
